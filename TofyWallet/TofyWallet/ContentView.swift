6d75919e368e604aeda3ee804f5db3144647bd1e
Mikel Lopez <mikel_lopez_salazar@carrefour.com>
2021-01-21 16:31:15 +0100

    project created

---
+++ TofyWallet/TofyWallet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TofyWallet
+//
+//  Created by usuario on 21/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TofyWallet/TofyWallet/TofyWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  TofyWalletApp.swift
+//  TofyWallet
+//
+//  Created by usuario on 21/1/21.
+//
+
+import SwiftUI
+
+@main
+struct TofyWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
